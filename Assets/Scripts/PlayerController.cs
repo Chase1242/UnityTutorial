8fbaaadca5a0c658369995238b733eb6de709447
Chase <chaseconaway82@gmail.com>
2020-10-19 16:07:30 -0700

    Added some more bug control and funtionality into the archer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     public int id = 0;
     bool fire = false;
 
-    public Animator animator;
+    public Animator topAnimator;
+    public Animator bottomAnimator;
     public GameObject crossHair;
 
     public GameObject arrowPrefab;
@@ -22,17 +23,27 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVertical"), 0.0f);
         Vector3 aim = new Vector3(Input.GetAxis("AimHorizontal"), Input.GetAxis("AimVertical"), 0.0f);
 
-        AimAndShoot(aim);
+        AimAndShoot();
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Magnitude", movement.magnitude);
+        bottomAnimator.SetFloat("Horizontal", movement.x);
+        bottomAnimator.SetFloat("Vertical", movement.y);
+        bottomAnimator.SetFloat("Magnitude", movement.magnitude);
+
+        topAnimator.SetFloat("MoveHorizontal", movement.x);
+        topAnimator.SetFloat("MoveVertical", movement.y);
+        topAnimator.SetFloat("MoveMagnitude", movement.magnitude);
+
+        topAnimator.SetFloat("AimHorizontal", aim.x);
+        topAnimator.SetFloat("AimVertical", aim.y);
+        topAnimator.SetFloat("AimMagnitude", aim.magnitude);
+        topAnimator.SetBool("Aim", fire);
 
         transform.position = transform.position + movement * Time.deltaTime;
     }
 
-    private void AimAndShoot(Vector2 aim)
-    { 
+    private void AimAndShoot()
+    {
+        Vector3 aim = new Vector3(Input.GetAxis("AimHorizontal"), Input.GetAxis("AimVertical"), 0.0f);
         Vector2 shootingDirection = new Vector2(Input.GetAxis("AimHorizontal"), Input.GetAxis("AimVertical"));
         
         if (aim.magnitude > 0.0f)
@@ -43,19 +54,19 @@ public class PlayerController : MonoBehaviour
             crossHair.SetActive(true);
 
             shootingDirection.Normalize();
-            if (Input.GetAxis("Fire") > 0.3f && !fire)
+            if (Input.GetAxis("Fire") > 0.3f)
+                fire = true;
+
+            if (Input.GetAxis("Fire") < 0.3f && fire)
             {
                 GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
                 arrow.GetComponent<Rigidbody2D>().velocity = shootingDirection * 3.0f;
                 arrow.transform.Rotate(0.0f, 0.0f, Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg);
-                fire = true;
                 Destroy(arrow, 2);
             }
 
-            if (Input.GetAxis("Fire") == 0.0)
-            {
+            if (Input.GetAxis("Fire") < 0.3f)
                 fire = false;
-            }
 
         } else
         {
